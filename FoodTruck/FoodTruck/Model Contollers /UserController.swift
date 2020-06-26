8b93b7960d39ad4ce0a95956fd6c2cc8eb159e74
Kevin Stewart <kistewart419@gmail.com>
2020-06-25 22:21:35 -0400

    Firebase controller

--- FoodTruck/FoodTruck/Model Contollers /UserController.swift
+++ FoodTruck/FoodTruck/Model Contollers /UserController.swift
@@ -153,7 +153,6 @@ class UserController {
         diner.password = representation.password
     }
 
-
     func deleteTaskFromServer(_ diner: Diner, completion: @escaping CompletionHandler = { _ in }) {
         guard let uuid = diner.identifier else {
             completion(.failure(.noIdentifier))
